066095310efc7acbd1b8c01f32e5eb4bee847791
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-12 13:34:17 -0700

    Add functionality to the Base Model to create an instance with the dictionary representation of the class we created before

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,21 @@ from datetime import datetime
 
 class BaseModel:
     """Class that defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.strptime(
+                        value,
+                        "%Y-%m-%dT%H:%M:%S.%f"
+                        ))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
