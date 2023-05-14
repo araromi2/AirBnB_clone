6e288acad7a2d504a49e1399cf62da3b3fd30fea
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-14 08:01:26 -0700

    Add console.py that contains the entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for HBNB"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program when EOF (Ctrl+D) is encountered"""
+        print("") # Print a new line before exiting
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
